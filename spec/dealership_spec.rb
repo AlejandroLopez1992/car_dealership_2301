e5af7a08c9d1e953d864cd3cbedcaf8c358db8e7
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-08 10:06:02 -0700

    Feat: complete add_car method

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe Dealership do
     expect(dealership.inventory_count).to eq(0)
   end
 
-  xit 'can add car instances to the inventory' do
+  it 'can add car instances to the inventory' do
     dealership = Dealership.new("Acme Auto", "123 Main Street")
     car_1 = Car.new("Ford Mustang", 1500, 36)
     car_2 = Car.new("Toyota Prius", 1000, 48)

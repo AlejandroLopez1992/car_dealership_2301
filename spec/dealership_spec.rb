e2550b5466ece005f84d28a9cbc5b0be5bd7e8c9
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-02-08 09:53:10 -0700

    Test: add needed tests for dealership class functionality

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -3,4 +3,33 @@ require './lib/car'
 require './lib/dealership'
 
 RSpec.describe Dealership do
+
+  it 'exists' do 
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership).to be_a Dealership
+  end
+
+  it 'has initial empty array of inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory).to eq([])
+  end
+
+  it 'inventory count is 0 when inventory is empty' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory_count).to eq(0)
+  end
+
+  it 'can add car instances to the inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+
+    expect(dealership.inventory).to eq([car_1, car_2])
+    expect(dealership.inventory_count).to eq(2)
+  end
 end
